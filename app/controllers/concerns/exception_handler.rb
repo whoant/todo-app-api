acb803185619fe44d2dbdd39598a141035475dba
Vo Van Hoang Tuan <19dh110082@st.huflit.edu.vn>
2021-11-24 20:36:53 +0700

    Route: todos/:todo_item/items

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,11 +3,16 @@ module ExceptionHandler
 
 	included do
 		rescue_from ActiveRecord::RecordNotFound do |e|
-			json_response({message: e.message}, :not_found)
+			json_response({ message: e.message }, :not_found)
 		end
 
 		rescue_from ActiveRecord::RecordInvalid do |e|
-			json_response({message: e.message}, :unprocessable_entity)
+			json_response({ message: e.message }, :unprocessable_entity)
+		end
+
+		rescue_from NoMethodError do |e|
+			p e
+			json_response({ message: 'Item does not exist' }, :unprocessable_entity)
 		end
 	end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,47 @@
 class ItemsController < ApplicationController
+	before_action :set_todo
+	before_action :set_todo_items, only: [:show, :update, :destroy]
+
+	# GET /todos/:todo_id
+	def index
+		json_response(@todo.items)
+	end
+
+	# GET /todos/:todo_id/items/:id
+	def show
+		json_response(@item)
+	end
+
+	# POST /todos/:todo_id
+	def create
+		@todo.items.create!(item_params)
+		json_response(@todo, :created)
+	end
+
+	# PUT /todos/:todo_id/items/:id
+	def update
+		@item.update(item_params)
+		head :no_content
+	end
+
+	# DELETE /todos/:todo_id/items/:id
+	def destroy
+		@item.destroy
+		head :no_content
+	end
+
+	private
+
+	def set_todo
+		@todo = Todo.find(params[:todo_id])
+	end
+
+	def item_params
+		params.permit(:name, :done)
+	end
+
+	def set_todo_items
+		@item = @todo.items.find_by(id: params[:id]) if @todo
+	end
 
 end
